1b7b4bcf50c3b0d674261c34fd6bd92aa584c9d6
Apisit (BDT) <bdtapp@ssi-steel.com>
2026-07-10 10:46:22 +0700

    [keycloak-theme] claim ownership of Template, Page, Form, SocialProviders for customization

---
+++ keycloak-theme/src/login/pages/login/Form.tsx
@@ -0,0 +1,226 @@
+/**
+ * This file has been claimed for ownership from @keycloakify/login-ui version 250004.7.2.
+ * To relinquish ownership and restore this file to its original content, run the following command:
+ * 
+ * $ npx keycloakify own --path "login/pages/login/Form.tsx" --revert
+ */
+
+import { useState } from "react";
+import { assert } from "tsafe/assert";
+import { PasswordWrapper } from "../../components/PasswordWrapper";
+import { useI18n } from "../../i18n";
+import { useKcContext } from "../../KcContext";
+import { useKcClsx } from "@keycloakify/login-ui/useKcClsx";
+import { kcSanitize } from "@keycloakify/login-ui/kcSanitize";
+import { useScript } from "./useScript";
+
+export function Form() {
+    const { kcContext } = useKcContext();
+    assert(kcContext.pageId === "login.ftl");
+
+    const { msg, msgStr } = useI18n();
+
+    const [isLoginButtonDisabled, setIsLoginButtonDisabled] = useState(false);
+
+    const { kcClsx } = useKcClsx();
+
+    const webAuthnButtonId = "authenticateWebAuthnButton";
+
+    useScript({ webAuthnButtonId });
+
+    return (
+        <>
+            <div id="kc-form">
+                <div id="kc-form-wrapper">
+                    {kcContext.realm.password && (
+                        <form
+                            id="kc-form-login"
+                            onSubmit={() => {
+                                setIsLoginButtonDisabled(true);
+                                return true;
+                            }}
+                            action={kcContext.url.loginAction}
+                            method="post"
+                        >
+                            {!kcContext.usernameHidden && (
+                                <div className={kcClsx("kcFormGroupClass")}>
+                                    <label htmlFor="username" className={kcClsx("kcLabelClass")}>
+                                        {!kcContext.realm.loginWithEmailAllowed
+                                            ? msg("username")
+                                            : !kcContext.realm.registrationEmailAsUsername
+                                              ? msg("usernameOrEmail")
+                                              : msg("email")}
+                                    </label>
+                                    <input
+                                        tabIndex={2}
+                                        id="username"
+                                        className={kcClsx("kcInputClass")}
+                                        name="username"
+                                        defaultValue={kcContext.login.username ?? ""}
+                                        type="text"
+                                        autoFocus
+                                        autoComplete={
+                                            kcContext.enableWebAuthnConditionalUI
+                                                ? "username webauthn"
+                                                : "username"
+                                        }
+                                        aria-invalid={kcContext.messagesPerField.existsError(
+                                            "username",
+                                            "password"
+                                        )}
+                                    />
+                                    {kcContext.messagesPerField.existsError("username", "password") && (
+                                        <span
+                                            id="input-error"
+                                            className={kcClsx("kcInputErrorMessageClass")}
+                                            aria-live="polite"
+                                            dangerouslySetInnerHTML={{
+                                                __html: kcSanitize(
+                                                    kcContext.messagesPerField.getFirstError(
+                                                        "username",
+                                                        "password"
+                                                    )
+                                                )
+                                            }}
+                                        />
+                                    )}
+                                </div>
+                            )}
+
+                            <div className={kcClsx("kcFormGroupClass")}>
+                                <label htmlFor="password" className={kcClsx("kcLabelClass")}>
+                                    {msg("password")}
+                                </label>
+                                <PasswordWrapper passwordInputId="password">
+                                    <input
+                                        tabIndex={3}
+                                        id="password"
+                                        className={kcClsx("kcInputClass")}
+                                        name="password"
+                                        type="password"
+                                        autoComplete="current-password"
+                                        aria-invalid={kcContext.messagesPerField.existsError(
+                                            "username",
+                                            "password"
+                                        )}
+                                    />
+                                </PasswordWrapper>
+                                {kcContext.usernameHidden &&
+                                    kcContext.messagesPerField.existsError("username", "password") && (
+                                        <span
+                                            id="input-error"
+                                            className={kcClsx("kcInputErrorMessageClass")}
+                                            aria-live="polite"
+                                            dangerouslySetInnerHTML={{
+                                                __html: kcSanitize(
+                                                    kcContext.messagesPerField.getFirstError(
+                                                        "username",
+                                                        "password"
+                                                    )
+                                                )
+                                            }}
+                                        />
+                                    )}
+                            </div>
+
+                            <div className={kcClsx("kcFormGroupClass", "kcFormSettingClass")}>
+                                <div id="kc-form-options">
+                                    {kcContext.realm.rememberMe && !kcContext.usernameHidden && (
+                                        <div className="checkbox">
+                                            <label>
+                                                <input
+                                                    tabIndex={5}
+                                                    id="rememberMe"
+                                                    name="rememberMe"
+                                                    type="checkbox"
+                                                    defaultChecked={!!kcContext.login.rememberMe}
+                                                />{" "}
+                                                {msg("rememberMe")}
+                                            </label>
+                                        </div>
+                                    )}
+                                </div>
+                                <div className={kcClsx("kcFormOptionsWrapperClass")}>
+                                    {kcContext.realm.resetPasswordAllowed && (
+                                        <span>
+                                            <a
+                                                tabIndex={6}
+                                                href={kcContext.url.loginResetCredentialsUrl}
+                                            >
+                                                {msg("doForgotPassword")}
+                                            </a>
+                                        </span>
+                                    )}
+                                </div>
+                            </div>
+
+                            <div id="kc-form-buttons" className={kcClsx("kcFormGroupClass")}>
+                                <input
+                                    type="hidden"
+                                    id="id-hidden-input"
+                                    name="credentialId"
+                                    value={kcContext.auth.selectedCredential}
+                                />
+                                <input
+                                    tabIndex={7}
+                                    disabled={isLoginButtonDisabled}
+                                    className={kcClsx(
+                                        "kcButtonClass",
+                                        "kcButtonPrimaryClass",
+                                        "kcButtonBlockClass",
+                                        "kcButtonLargeClass"
+                                    )}
+                                    name="login"
+                                    id="kc-login"
+                                    type="submit"
+                                    value={msgStr("doLogIn")}
+                                />
+                            </div>
+                        </form>
+                    )}
+                </div>
+            </div>
+
+            {kcContext.enableWebAuthnConditionalUI && (
+                <>
+                    <form id="webauth" action={kcContext.url.loginAction} method="post">
+                        <input type="hidden" id="clientDataJSON" name="clientDataJSON" />
+                        <input type="hidden" id="authenticatorData" name="authenticatorData" />
+                        <input type="hidden" id="signature" name="signature" />
+                        <input type="hidden" id="credentialId" name="credentialId" />
+                        <input type="hidden" id="userHandle" name="userHandle" />
+                        <input type="hidden" id="error" name="error" />
+                    </form>
+                    {kcContext.authenticators !== undefined &&
+                        kcContext.authenticators.authenticators.length !== 0 && (
+                            <>
+                                <form id="authn_select" className={kcClsx("kcFormClass")}>
+                                    {kcContext.authenticators.authenticators.map((authenticator, i) => (
+                                        <input
+                                            key={i}
+                                            type="hidden"
+                                            name="authn_use_chk"
+                                            readOnly
+                                            value={authenticator.credentialId}
+                                        />
+                                    ))}
+                                </form>
+                            </>
+                        )}
+                    <br /> {/* We use a br here because kcMarginTopClass is not defined in login v1 */}
+                    <input
+                        id={webAuthnButtonId}
+                        type="button"
+                        className={kcClsx(
+                            "kcButtonClass",
+                            "kcButtonDefaultClass",
+                            "kcButtonBlockClass",
+                            "kcButtonLargeClass"
+                        )}
+                        value={msgStr("passkey-doAuthenticate")}
+                    />
+                </>
+            )}
+        </>
+    );
+}
